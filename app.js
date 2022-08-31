0f68fab0f050677523966ac6833782da3b85aae1
Shruti Roy <shruti.2018.r@gmail.com>
2022-08-31 19:08:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ next.onclick = (e) => {
     changeSong(i)
     setTimeout(function(){
         duration()
-    }, 100);
+    }, 1000);
 };
 
 prev.onclick = (e) => {
@@ -44,7 +44,7 @@ prev.onclick = (e) => {
     changeSong(i)
     setTimeout(function(){
         duration()
-    }, 100);
+    }, 1000);
 };
 
 //PLAY-PAUSE BUTTONS
